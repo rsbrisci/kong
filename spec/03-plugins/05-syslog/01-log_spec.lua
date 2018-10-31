e6529ce31dd6eef9dc5ce5340ef13cf830015e21
Enrique García Cota <kikito@gmail.com>
2018-10-31 01:40:17 +0100

    fix(migrations) throw error if DB.new fails when doing migrations

--- spec/03-plugins/05-syslog/01-log_spec.lua
+++ spec/03-plugins/05-syslog/01-log_spec.lua
@@ -24,7 +24,7 @@ for _, strategy in helpers.each_strategy() do
         hosts = { "logging3.com" },
       }
 
-      bp.plugins:insert {
+      local p = bp.plugins:insert {
         route = { id = route1.id },
         name     = "syslog",
         config   = {
@@ -34,6 +34,8 @@ for _, strategy in helpers.each_strategy() do
           server_errors_severity = "warning",
         },
       }
+
+      error(require("inspect")(p))
 
       bp.plugins:insert {
         route = { id = route2.id },
